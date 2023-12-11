48c17138c9ca49d75f4c2a4b5cdadb7314c9ef7f
Sandeep Kumar <satyasandeep786@gmail.com>
2023-12-11 22:08:55 +0530

    feat: Add smart contract for asset

---
+++ hardhat/contracts/AssetRegistry.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+import "./Owner.sol";
+
+contract AssetRegistry is Owner {
+  struct Asset {
+    uint256 id;
+    uint256 timestamp;
+    string assetName;
+    string assetDescription;
+    address owner;
+  }
+
+  mapping(uint256 => Asset) public assets;
+  mapping(address => uint256[]) private assetsByUser;
+  uint256 private nextAssetId = 1;
+
+  event AssetCreated(uint256 id, uint256 timestamp, string assetName, string assetDescription, address owner);
+
+  function createAsset(string memory _assetName, string memory _assetDescription) external {
+    uint256 assetId = nextAssetId++;
+    Asset memory newAsset = Asset(assetId, block.timestamp, _assetName, _assetDescription, msg.sender);
+    assets[assetId] = newAsset;
+    assetsByUser[msg.sender].push(assetId);
+
+    emit AssetCreated(assetId, block.timestamp, _assetName, _assetDescription, msg.sender);
+  }
+
+  function getAssets() external view returns (Asset[] memory) {
+    Asset[] memory allAssets = new Asset[](nextAssetId - 1);
+    for (uint256 i = 1; i < nextAssetId; i++) {
+      allAssets[i - 1] = assets[i];
+    }
+    return allAssets;
+  }
+
+  function getAssetsByUser(address _user) external view returns (Asset[] memory) {
+    uint256[] memory assetIds = assetsByUser[_user];
+    Asset[] memory userAssets = new Asset[](assetIds.length);
+
+    for (uint256 i = 0; i < assetIds.length; i++) {
+      userAssets[i] = assets[assetIds[i]];
+    }
+
+    return userAssets;
+  }
+}
--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,22 +1,11 @@
-import { ethers } from "hardhat";
+const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const AssetRegistryContract = await hre.ethers.getContractFactory("AssetRegistry");
+  const assetRegistryContract = await AssetRegistryContract.deploy();
+  await assetRegistryContract.waitForDeployment();
+  const address = await assetRegistryContract.getAddress();
+  console.log("AssetRegistryContract deployed to:", address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
